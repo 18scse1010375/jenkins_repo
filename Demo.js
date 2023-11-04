44ebfdf4b8fb91ebf2dfc02c8042fd55c7d521a9
Arun Singhal <singhalarun03@gmail.com>
2023-11-04 16:57:56 +0530

    Add one JS File

---
+++ Demo.js
@@ -0,0 +1,2 @@
+console.log("This is a first javascript file")
+console.log("This file is created by Arun Singhal")
--- First.java
+++ First.java
@@ -1,17 +1,13 @@
 import java.util.*;
 public class First{
 public static void main(String[]args){
-int a,b;
-
-Scanner sc=new Scanner(System.in);
 System.out.println("Hello world");
 System.out.println("Hello Arun !! How are you ");
 System.out.println("Enter First Number:");
-a=sc.nextInt();
-System.out.println("Enter Second Number:");
-b=sc.nextInt();
 
-System.out.println("Sum of two mumber: " + (a+b) );
+System.out.println("Enter Second Number:");
+
+
 
 
 }
